7ae8cddaf6f17695920b4561f0e6fff049cab7a5
Arthur Correnson <arthur.correnson@gmail.com>
2020-05-15 17:08:24 +0200

    Add the repl

---
+++ bin/repl.ml
@@ -0,0 +1,22 @@
+open Kernel.Tactic
+open Language.Parser
+
+let repl prop =
+  let rec step ctx =
+    match ctx.goals with
+    | [] -> qed prop ctx
+    | _ ->
+      ignore (Sys.command "clear");
+      ignore (debug ctx);
+      print_string "> ";
+      flush stdout;
+      match command (read_line ()) with
+      | Some f -> step (f ctx)
+      | None -> step ctx
+  in
+  step (init prop)
+
+let _ =
+  repl (Impl (Or (Atom 'a', Atom 'b'), Or (Atom 'b', Atom 'a')))
+
+
---
+++ src/language/parser.ml
@@ -0,0 +1,26 @@
+open Kernel.Logic
+open Kernel.Tactic
+open Opal
+
+let parens = between (exactly '(') (exactly ')')
+
+let p_impl = token "->" >> return (fun p q -> Impl (p, q))
+let p_and = token "/\\" >> return (fun p q -> And (p, q))
+let p_or = token "\\/" >> return (fun p q -> Or (p, q))
+let p_atom = spaces >> letter => (fun x -> Atom x)
+
+let rec p_prop inp = chainr1 p_disj p_impl inp
+and p_disj inp = chainl1 p_conj p_or inp
+and p_conj inp = chainl1 p_min p_and inp
+and p_min inp = (parens p_prop <|> p_atom) inp
+
+let command str = parse
+    (choice [
+        token "elim" >> p_prop => elim;
+        token "intro" >> return intro;
+        token "left" >> return left;
+        token "right" >> return right;
+        token "debug" >> return debug;
+        token "assumption" >> return assumption;
+      ])
+    (LazyStream.of_string str)
